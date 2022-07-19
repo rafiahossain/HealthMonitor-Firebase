1fc6b9607d7570d9f66a03190edc89ed2fdfa4f5
rafiahossain <rafiahossain@student.usm.my>
2022-07-20 00:59:01 +0800

    First Commit Health Monitor

---
+++ app/src/main/java/com/example/tooth/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.tooth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Variable to set time for the splash screen
+    private static int SPLASH_SCREEN = 5000;
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView banner, descript;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //To remove status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //xml items
+        logo = findViewById(R.id.logo);
+        banner = findViewById(R.id.banner);
+        descript = findViewById(R.id.descript);
+
+        //Setting animations to xml items
+        logo.setAnimation(topAnim);
+        banner.setAnimation(bottomAnim);
+        descript.setAnimation(bottomAnim);
+
+        //Starting new activity, after delay set by variable SPLASH_SCREEN
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+
+                //Applying transition animation
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(logo, "logo_image");
+                pairs[1] = new Pair<View, String>(banner, "logo_text");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                }
+            }
+        }, SPLASH_SCREEN);
+
+    }
+
+}
